eaebe5a6f6f9885d7b5602917fe58f44a4226de1
Sayani Koley <38989758+sayani2014@users.noreply.github.com>
2021-06-08 11:53:59 +0530

    Update prime_factorization.sh

--- Day6/Prob1/prime_factorization.sh
+++ Day6/Prob1/prime_factorization.sh
@@ -1,6 +1,5 @@
 #! /bin/bash -x
 
-count=1
 read -p "Enter any number to print its prime factors: " number
 
 for (( i=2;i<=$number;i++ ))
@@ -15,20 +14,12 @@ do
 			if (( $b==0 ))
 			then
 				isPrime=0
-				break
+				exit 0
 			fi
 		done
 		if [ $isPrime -eq 1 ]
 		then
-			#echo "$i"
-			
-			result[$count]=$i
-			count=$(($count+1))
+			echo "$i"
 		fi
 	fi
 done
-
-if [[ $isPrime -eq 0 || $isPrime -eq 1 ]]
-then
-	echo "Random numbers: " "${result[*]}"
-fi
